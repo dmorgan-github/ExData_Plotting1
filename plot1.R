7b844c8dcbf5d842cfb283983abb856277c65f1e
David Morgan <david@clairvoyantsoft.com>
2015-05-09 10:18:30 -0700

    commiting assignment

---
+++ plot1.R
@@ -0,0 +1,61 @@
+########################################################################
+# Note the script requires dplyr and lubridate packages to be installed
+# To install, run the following lines:
+# install.packages("dplyr")
+# install.packages("lubridate")
+# The script also expects a data file named household_power_consumption.txt
+# to exist in the same directory as the script.
+# If the file is not present you will be prompted to download
+# the file so the script can run.
+########################################################################
+
+library(dplyr)
+library(lubridate)
+
+file_name = "household_power_consumption.txt"
+
+download <- function () {
+  download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", "power_consumption.zip", method="curl")
+  utils::unzip("power_consumption.zip")
+}
+
+makeplot <- function(df) {
+  par(mfrow = c(1,1))
+  hist(df$Global_active_power,main="Global Active Power", xlab="Global Active Power (kilowats)", col="Red")
+}
+
+run <- function() {
+  
+  print ("Making plot 1...")
+  # load the data
+  df <- read.table(file_name, sep=";", header=TRUE, na.strings=c("?"), skip=66635, nrows=2880)
+  colnames(df) <- c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
+  df <- mutate(df, datetime = dmy_hms(paste(Date, Time)))
+  
+  # print plot to screen
+  makeplot(df)
+  
+  #print plot to png file
+  print("Writing the plot to plot1.png...")
+  png("plot1.png", width=480, height=480)
+  makeplot(df)
+  dev.off()
+}
+
+#ensure file is available
+if (file.exists(file_name)) {
+  run()
+} else {
+  yn <- readline(prompt="Data file does not exist. Would you like to download it? Y/N ")
+  if (tolower(yn) == "y") {
+    download()
+    run()
+  } else {
+    print("Unable to run the program")
+  }
+}
+
+
+
+
+
---
+++ plot4.R
@@ -0,0 +1,70 @@
+########################################################################
+# Note the script requires dplyr and lubridate packages to be installed
+# To install, run the following lines:
+# install.packages("dplyr")
+# install.packages("lubridate")
+# The script also expects a data file named household_power_consumption.txt
+# to exist in the same directory as the script.
+# If the file is not present you will be prompted to download
+# the file so the script can run.
+########################################################################
+
+library(dplyr)
+library(lubridate)
+
+file_name = "household_power_consumption.txt"
+
+download <- function () {
+  download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", "power_consumption.zip", method="curl")
+  utils::unzip("power_consumption.zip")
+}
+
+makeplot <- function(df) {
+  par(mfrow = c(2,2))
+  plot(df$Global_active_power ~ df$datetime, type="l", xlab="", ylab="Global Active Power")
+  plot(df$Voltage ~ df$datetime, type="l", xlab="datetime", ylab="Voltage")
+  
+  plot(df$Sub_metering_1 ~ df$datetime, type="l", xlab="", ylab="Energy sub metering")
+  lines(df$Sub_metering_2 ~ df$datetime,col="Red")
+  lines(df$Sub_metering_3 ~ df$datetime, col="Blue")
+  legend("topright", col=c("Black", "Red", "Blue"), bty="n", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lwd=1)
+  
+  plot(df$Global_reactive_power ~ df$datetime, type="l", xlab="datetime", ylab="Global_reactive_power")
+  par(mfrow = c(1,1))
+}
+
+run <- function() {
+  
+  print ("Making plot 4...")
+  # load the data
+  df <- read.table(file_name, sep=";", header=TRUE, na.strings=c("?"), skip=66635, nrows=2880)
+  colnames(df) <- c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
+  df <- mutate(df, datetime = dmy_hms(paste(Date, Time)))
+  
+  # print plot to screen
+  makeplot(df)
+  
+  #print plot to png file
+  print("Writing the plot to plot4.png...")
+  png("plot4.png", width=480, height=480)
+  makeplot(df)
+  dev.off()
+}
+
+#ensure file is available
+if (file.exists(file_name)) {
+  run()
+} else {
+  yn <- readline(prompt="Data file does not exist. Would you like to download it? Y/N ")
+  if (tolower(yn) == "y") {
+    download()
+    run()
+  } else {
+    print("Unable to run the program")
+  }
+}
+
+
+
+
+
